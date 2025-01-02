588e489321fad25f1cda65ec3369410e7fa6e489
devaleriofrancesco <servizi@francescodevalerio.com>
2025-01-02 13:19:09 +0100

    BE - booking delete action

--- src/main/java/com/devaleriofrancesco/easystay/controller/BookingController.java
+++ src/main/java/com/devaleriofrancesco/easystay/controller/BookingController.java
@@ -7,9 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,11 +18,29 @@ public class BookingController {
 
     private final BookingService bookingService;
 
+    // get all bookings by specific user
     @GetMapping("")
     public ResponseEntity<List<Booking>> myBookings() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User user = (User) authentication.getPrincipal();
         return ResponseEntity.ok(bookingService.findByCliente(user));
+    }
+
+    // Change booking check-in and check-out dates
+    @PutMapping("/change-dates/{id}")
+    public ResponseEntity<Booking> changeDates(@PathVariable Integer id, @RequestBody Booking booking) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
+        return ResponseEntity.ok(bookingService.changeDates(id, booking, user));
+    }
+
+    // delete booking by id checking if the user is the owner
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBooking(@PathVariable Integer id) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
+        bookingService.deleteBooking(id, user);
+        return ResponseEntity.noContent().build();
     }
 
 }
